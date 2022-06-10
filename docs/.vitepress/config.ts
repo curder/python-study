2a84c94b7555bda82878163b3ad04da841924591
curder <q.curder@gmail.com>
2022-06-10 18:17:15 +0800

    Change directory name

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -21,38 +21,48 @@ export default defineConfig({
         ],
         nav: nav(),
         sidebar: {
-            '/basic': sidebarBasic(),
+            "/guide": sidebarGuide(),
         },
     }
 });
 
 function nav() {
     return [
-        {text: '基础', link: '/basic/install-python', activeMatch: '/basic/'},
+        {text: 'Guide', link: '/guide/install-python', activeMatch: '/guide/'},
     ];
 }
 
-function sidebarBasic() {
+function sidebarGuide() {
     return [
         {
             text: "基础",
+            collapsible: true,
+            collapsed: false,
             items: [
-                {text: "安装", link: "/basic/install-python"},
-                {text: "print函数", link: "/basic/function-print"},
-                {text: "转义字符", link: "/basic/escape-character"},
-                {text: "系统保留字", link: "/basic/reserved-word"},
-                {text: "变量的定义和使用", link: "/basic/variable"},
-                {text: "数据类型", link: "/basic/type-of-data"},
-                {text: "数据类型转换", link: "/basic/data-type-conversion"},
-                {text: "代码注释", link: "/basic/comments"},
-                {text: "input函数", link: "/basic/function-input"},
-                {text: "运算符", link: "/basic/operator"},
-                {text: "流程判断", link: "/basic/if-elif-else"},
-                {text: "pass语句", link: "/basic/pass"},
-                {text: "range函数", link: "/basic/function-range"},
-                {text: "循环结构", link: "/basic/while-and-for-in"},
-                {text: "else语句", link: "/basic/else"}
+                {text: "安装", link: "/guide/install-python"},
+                {text: "print函数", link: "/guide/function-print"},
+                {text: "转义字符", link: "/guide/escape-character"},
+                {text: "系统保留字", link: "/guide/reserved-word"},
+                {text: "变量的定义和使用", link: "/guide/variable"},
+                {text: "数据类型", link: "/guide/type-of-data"},
+                {text: "数据类型转换", link: "/guide/data-type-conversion"},
+                {text: "代码注释", link: "/guide/comments"},
+                {text: "input函数", link: "/guide/function-input"},
+                {text: "运算符", link: "/guide/operator"},
+                {text: "流程判断", link: "/guide/if-elif-else"},
+                {text: "pass语句", link: "/guide/pass"},
+                {text: "range函数", link: "/guide/function-range"},
+                {text: "循环结构", link: "/guide/while-and-for-in"},
+                {text: "else语句", link: "/guide/else"}
             ]
         },
+        {
+            text: "列表",
+            collapsible: true,
+            collapsed: false,
+            items: [
+                {text: "创建列表", link: "/guide/list/create"}
+            ]
+        }
     ];
 }
