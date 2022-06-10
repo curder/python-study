0940a2bfd43332b92531864eae6ad6762b281a01
curder <curder@users.noreply.github.com>
2022-06-10 06:32:30 +0000

    deploy: 66703f654f825f92244c148811b9c6e50c7e2eae

---
+++ assets/basic_pass.md.4452d366.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as a,o as n,a as p}from"./app.cdc0c4f1.js";const d='{"title":"pass\u8BED\u53E5","description":"","frontmatter":{},"headers":[],"relativePath":"basic/pass.md"}',l={name:"basic/pass.md"},e=p("",3),o=[e];function t(c,r,i,D,y,F){return n(),a("div",null,o)}var A=s(l,[["render",t]]);export{d as __pageData,A as default};
