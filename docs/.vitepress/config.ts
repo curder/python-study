0d95775c746cc7f3e68c29b23139a997eafd22c2
curder <q.curder@gmail.com>
2022-06-28 17:58:02 +0800

    update set group collapsed

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -153,7 +153,7 @@ function sidebarGuide() {
         {
             text: "文件系统",
             collapsible: true,
-            collapsed: false,
+            collapsed: true,
             items: [
                 {text: "文件编码", link: "/guide/os/encoding"},
                 {text: "常用方法", link: "/guide/os/method"},
