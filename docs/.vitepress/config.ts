00ce5d27415a19accd95212a5271c3a5eebfb104
curder <q.curder@gmail.com>
2022-08-04 22:15:18 +0800

    Add file

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -167,23 +167,10 @@ function sidebarGuide() {
                 {text: "文件编码", link: "/guide/os/encoding"},
                 {text: "常用方法", link: "/guide/os/method"},
                 {text: "with语句", link: "/guide/os/with"},
+                {text: "文件操作", link: "/guide/os/file"},
                 {text: "目录操作", link: "/guide/os/directory"},
             ],
         },
-    ];
-}
-
-
-function sidebarCase() {
-    return [
-        {
-            text: "磁盘IO",
-            collapsible: true,
-            collapsed: false,
-            items: [
-                {text: "IO", link: "/case/io"},
-            ],
-        }
     ];
 }
 
